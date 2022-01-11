22a14979903db1c560e723e88d5887edf8a305fb
mgrobstein <97206147+mgrobstein@users.noreply.github.com>
2022-01-11 13:59:41 -0800

    completed the assignment

--- index.js
+++ index.js
@@ -125,19 +125,20 @@ function hungryDog(weight, age){
       return weight*0.05;
     }
   }
-  else if (age >= (7/12)){
+  else if (age >= (0.583)){
     return weight * 0.04;
   }
-  else if (age >= (4/12)){
+  else if (age >= (0.333)){
     return weight * 0.05;
   }
-  else if (age >= (2/12)){
+  else if (age >= (0.167)){
     return weight * 0.1
   }
   else{
     return 'Puppies that are younger than eight weeks are too young to leave their mothers'
   }
 }
+console.log(hungryDog(4,0.583))
 
 
 
@@ -163,49 +164,52 @@ RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors
 
 HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
-const computer = Math.floor(Math.random() * 3);
-const user = "Paper";
+let computer = Math.floor(Math.random() * 3);
+if (computer === 0){
+  computer = 'rock'
+}
+else if (computer === 1){
+  computer = 'paper'
+}
+else {
+  computer = 'scissors'
+}
 function game(user, computer){
-  if (user === "Rock"){
-    if(computer === 0) {
+  if (user === "rock"){
+    if(computer === 'rock') {
       return "it's a tie";
     }
-    else if (computer === 1){
+    else if (computer === 'paper'){
       return "you lose!";
     }
-    else if (computer === 2){
+    else if (computer === 'scissors'){
       return "you win!";
     }
   }
-  else if (user === "Scissors"){
-    if(computer === 0) {
+  else if (user === "scissors"){
+    if(computer === 'rock') {
       return "you lose!";
     }
-    else if (computer === 1){
+    else if (computer === 'paper'){
       return "you win!";
     }
-    else if (computer === 2){
+    else if (computer === 'scissors'){
       return "it's a tie";
     }
   }
-  else if (user === "Paper"){
-    if(computer === 0) {
+  else{
+    if(computer === 'rock') {
       return "you win!";
     }
-    else if (computer === 1){
+    else if (computer === 'paper'){
       return "it's a tie";
     }
-    else if (computer === 2){
+    else if (computer === 'scissors'){
       return "you lose!";
     }
-}
-else{
-  return "Your input was invalid"
-}
-}
-console.log(computer);
-console.log(user);
-console.log(game(user,computer));
+};
+};
+
 
 
 
@@ -220,8 +224,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(kilometers){
+  return kilometers * 0.621371;
 }
 
 
@@ -234,8 +238,8 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
 
@@ -249,9 +253,12 @@ Using the annoyingSong function below do the following:
 2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
-
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(bottlesOfSoda){
+  for (let i=0;i<bottlesOfSoda;i++){
+  let currentBottles = bottlesOfSoda;
+  bottlesOfSoda --;
+ return currentBottles+" bottles of soda on the wall, "+currentBottles+" bottles of soda, take one down pass it around "+bottlesOfSoda+" bottles of soda on the wall"
+}
 }
 
 
@@ -270,9 +277,24 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+if (score >= 90){
+  return 'you got an A';
 }
+else if (score >= 80){
+  return 'you got a B';
+}
+else if (score >= 70){
+  return 'you got a C';
+}
+else if (score >= 60){
+  return 'you got a D';
+}
+else{
+  return 'you got an F'
+}
+}
+console.log(grade(85))
 
 
 
@@ -289,8 +311,14 @@ HINT - try looking up the .includes() method
 */
 
 
-function vowelCounter(/*add your code here*/) {
-  /*add your code here*/
+function vowelCounter(string) {
+  let counter = 0;
+  for (let i=0; i<(string.length());i++){
+    if (string[i] === 'a' || string[i] === 'A' || string[i] === 'e' || string[i] === 'E' || string[i] === 'i' || string[i] === 'I' || string[i] === 'o' || string[i] === 'O' ||string[i] === 'u' ||string[i] === 'U' ||string[i] === 'y' ||string[i] === 'Y'){
+      counter ++;
+    }
+    return counter;
+  }
 }
 
 
